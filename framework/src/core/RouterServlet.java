4d2e3ac0fa2fe371e5f9c1633ebb9ae1de916960
RAEnomentsoa <mbolatianavelo@gmail.com>
2025-11-01 09:42:21 +0300

    controller
    list name controller

--- framework/src/core/RouterServlet.java
+++ framework/src/core/RouterServlet.java
@@ -5,6 +5,8 @@ import core.annotation.Route;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -14,20 +16,63 @@ public class RouterServlet extends HttpServlet {
 
     public final Map<String, Method> routes = new HashMap<>();
     public final Map<String, Object> controllers = new HashMap<>();
+    
+    //test d'un controller
+    // @Override 
+    // public void init() {
+    //     System.out.println(" Router initialized");
+    //     try {
+    //         Class<?> controllerClass = Class.forName("app.controllers.TestController");
+    //         Object controller = controllerClass.getDeclaredConstructor().newInstance();
+    //         registerController(controller); 
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
-    @Override 
-    public void init() {
-        System.out.println(" Router initialized");
-        try {
-            Class<?> controllerClass = Class.forName("app.controllers.TestController");
-            Object controller = controllerClass.getDeclaredConstructor().newInstance();
-            registerController(controller);
-        } catch (Exception e) {
-            e.printStackTrace();
+    // scanner tout les controller
+    @Override
+public void init() {
+    System.out.println("Router initialized");
+    String basePackage = "app.controllers";
+    String path = getServletContext().getRealPath("/WEB-INF/classes/" + basePackage.replace('.', '/'));
+
+    File directory = new File(path);
+    if (!directory.exists()) {
+        System.out.println("Controllers directory not found: " + path);
+        return;
+    }
+
+    scanAndRegisterControllers(directory, basePackage);
+}
+
+            private void scanAndRegisterControllers(File directory, String basePackage) {
+    File[] files = directory.listFiles();
+    if (files == null) return;
+
+    for (File file : files) {
+        if (file.isDirectory()) {
+            scanAndRegisterControllers(file, basePackage + "." + file.getName());
+        } else if (file.getName().endsWith(".class")) {
+            String className = file.getName().replace(".class", "");
+            try {
+                Class<?> clazz = Class.forName(basePackage + "." + className);
+
+                // Optional: check annotation like @Controller
+                if (clazz.getSimpleName().endsWith("Controller")) {
+                    Object controller = clazz.getDeclaredConstructor().newInstance();
+                    registerController(controller);
+                    System.out.println("Registered: " + clazz.getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
-              // Alernative: afaka manao fonction scan ny package iray manontolo ho an'ny controllers
-               // sy ny routes ao anatiny mba hiregister-ena azy ireo rehetra
+}
+    
+
+    // register controller
     public void registerController(Object controller) {
         Class<?> clazz = controller.getClass();
         if (clazz.isAnnotationPresent(Controller.class)) {
